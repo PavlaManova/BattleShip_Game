a436275b90d07f279f30296c2cb6f7caba30cdf3
PavlaManova <48806251+PavlaManova@users.noreply.github.com>
2020-12-28 02:49:36 +0200

    added startMenu

--- BattleShip_Game/BattleShip_Game.cpp
+++ BattleShip_Game/BattleShip_Game.cpp
@@ -17,40 +17,66 @@
 #include <iostream>
 #include <conio.h>
 #include <stdlib.h>
+#include <iomanip>
 
 using namespace std;
 
-void firstOption()
-{
-	cout << "           >Option 1<             " << endl;
-	cout << "            Option 2              " << endl;
-	cout << "            Option 3              " << endl;
-}
+const int MIDDLE_CONSOLE = 60;
 
-void secondOption()
-{
-	cout << "            Option 1              " << endl;
-	cout << "           >Option 2<             " << endl;
-	cout << "            Option 3              " << endl;
-}
+void startGameOption();
+void exitOption();
+void startingMenu();
+void setWhiteSpacesLength(string firstLine, string secondLine, int& whiteSpacesFirst, int& whiteSpacesSecond);
 
-void thirdOption()
-{
-	cout << "            Option 1             " << endl;
-	cout << "            Option 2             " << endl;
-	cout << "           >Option 3<            " << endl;
-}
+void printOptions(string firstOption, string secondOption);
 
 
 
 int main()
 {
+	
+	startingMenu();
 
-	firstOption();
+}
 
-	int choice = _getch();    
-	int temp = 3;   
+void setWhiteSpacesLength(string firstLine, string secondLine, int& whiteSpacesFirst, int& whiteSpacesSecond)
+{
+	whiteSpacesFirst = MIDDLE_CONSOLE + (firstLine.size() / 2);
 
+	int temp = firstLine.size() - secondLine.size();
+	if (secondLine.size() % 2 == 1)
+		temp++;
+
+	whiteSpacesSecond = whiteSpacesFirst - temp / 2;
+}
+
+void printOptions(string firstOption, string secondOption)
+{
+	cout << "\n\n\n\n\n\n\n\n\n";
+	int whiteSpacesFirst = 0, whiteSpacesSecond = 0;
+	setWhiteSpacesLength(firstOption, secondOption, whiteSpacesFirst, whiteSpacesSecond);
+	cout << setw(whiteSpacesFirst) << firstOption << endl;
+	cout << setw(whiteSpacesSecond) << secondOption << endl;
+}
+
+void startGameOption()
+{
+	printOptions(">New Game<", "Exit");	
+}
+
+void exitOption()
+{
+	printOptions("New Game", ">Exit<");
+}
+
+void startingMenu()
+{
+	startGameOption();
+
+	int choice = _getch(),
+		temp = 2;
+
+	bool endGame = false;
 	while (choice != 13) // Enter - choice is made
 	{
 		switch (choice)
@@ -68,24 +94,24 @@ int main()
 		default: break;
 		}
 
-		if (temp % 3 == 0)
+		if (temp % 2 == 0)
 		{
 			system("CLS");
-			firstOption();
-		}
-		else if (temp % 3 == 1)
-		{
-			system("CLS");
-			secondOption();
+			startGameOption();
+			endGame = false;
 		}
 		else
 		{
 			system("CLS");
-			thirdOption();
+			exitOption();
+			endGame = true;
 		}
 
 		choice = _getch();
-
 	}
 
+	if (endGame)
+		exit(0);
+	else
+		cout << "Start" << endl;
 }
